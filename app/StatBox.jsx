5dc30942caa04eb2365ed0739b50fce3c89763ba
Gail Terman <gterman@gmail.com>
2019-09-18 22:36:03 -0400

    add statbox

---
+++ app/StatBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './StatBox.sass';
+
+function Stat({name, value}) {
+    return (
+        <div className="stat">
+            <span className="name">{name}</span>
+            <span className="value">{value}</span>
+        </div>
+    );
+}
+
+export default class StatBox extends React.Component {
+    render() {
+        const stats = Object.keys(this.props.stats).map(k => <Stat name={k} value={this.props.stats[k]} />)
+        return <div className="stat-box">{stats}</div>
+    }
+
+}
--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GameList from './GameList';
+import StatBox from './StatBox';
 
 const fakeGames = [
     { title: 'Azul', checkouts: 29, available: true},
@@ -10,6 +11,14 @@ const fakeGames = [
     { title: 'Trouble', checkouts: 3, available: false}
 ]
 
+const fakeStats = {
+    titles: 963,
+    active_checkouts: 21,
+    pct_checked_out: 89,
+    titles_played: 384,
+    total_checkouts: 204,
+    hours_played: 9064
+}
 
 class App extends React.Component {
     render() {
@@ -17,6 +26,7 @@ class App extends React.Component {
             <div className="wrapper">
                 <h1>PAX Tabletop</h1>
                 <GameList games={fakeGames} />
+                <StatBox stats={fakeStats} />
             </div>
         );
     }
